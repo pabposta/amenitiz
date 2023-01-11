0367f67268b0558b4849a113fafd1431a2fe3e14
Pablo Stahl <stahl@dabbel.eu>
2023-01-11 10:35:45 +0100

    add basket add_item and (get) line_items (their test rely on each other) methods. pricing is ignored for now

--- app/basket.rb
+++ app/basket.rb
@@ -1,18 +1,44 @@
 # frozen_string_literal: true
 # typed: true
 
+require_relative 'item_service'
+
 require 'sorbet-runtime'
 class Basket
   extend T::Sig
 
+  sig { params(item_service: ItemService).void }
+  def initialize(item_service:)
+    @item_service = item_service
+    @line_items_by_code = {}
+  end
+
   sig { params(item_code: String).void }
-  def add_item(item_code:); end
+  def add_item(item_code:)
+    raise ArgumentError, "Item code #{item_code} does not exist" unless @item_service.exists?(item_code:)
+
+    unless @line_items_by_code.include?(item_code)
+      # if the item is not in the basket yet, add it. for existing items, we only increase the count
+      item = @item_service.item(item_code:)
+      @line_items_by_code[item_code] = LineItem.new(item:)
+    end
+    line_item = @line_items_by_code[item_code]
+    line_item.count += 1
+    line_item.total_discounted_price = 0.0
+  end
 
   sig { params(item_code: String).void }
   def remove_item(item_code:); end
 
   sig { returns(T::Array[LineItem]) }
-  def line_items; end
+  def line_items
+    # The alphabetical sort makes the order consistent. This is useful in testing, to ensure tests pass, and for a
+    # better user experience, so that the line items are always shown in the same order (although the UI itself
+    # could/should take care of this).
+    @line_items_by_code.keys.sort.map do |item_code|
+      @line_items_by_code[item_code]
+    end
+  end
 
   sig { returns(Float) }
   def total_discounted_price; end
--- test/basket_spec.rb
+++ test/basket_spec.rb
@@ -4,14 +4,30 @@ require 'rspec'
 require 'rspec/sorbet'
 require_relative '../app/basket'
 
+RSpec::Sorbet.allow_doubles!
+
 RSpec.describe Basket do
-  subject(:basket) { Basket.new }
+  let(:item1) { instance_double(Item, code: 'ITM1') }
+  let(:item2) { instance_double(Item, code: 'ITM2') }
+  let(:item_service) { instance_double(ItemService, exists?: true) }
+  subject(:basket) { Basket.new(item_service:) }
 
   describe '#add_item' do
     it 'allows to add items' do
+      expect(item_service).to receive(:item).with({ item_code: 'ITM1' }).and_return(item1)
+      basket.add_item(item_code: 'ITM1')
+      basket.add_item(item_code: 'ITM1')
+      expect(item_service).to receive(:item).with({ item_code: 'ITM2' }).and_return(item2)
+      basket.add_item(item_code: 'ITM2')
+      expect(basket.line_items).to eq([
+                                        LineItem.new(item: item1, count: 2, total_discounted_price: 0.0),
+                                        LineItem.new(item: item2, count: 1, total_discounted_price: 0.0)
+                                      ])
     end
 
     it 'raises an error when trying to add an item that does not exist' do
+      expect(item_service).to receive(:exists?).and_return(false)
+      expect { basket.add_item(item_code: 'ITM0') }.to raise_error(ArgumentError, 'Item code ITM0 does not exist')
     end
   end
 
@@ -24,7 +40,15 @@ RSpec.describe Basket do
   end
 
   describe '#line_items' do
-    it 'returns the line items' do
+    it 'returns the line items sorted alphabetically' do
+      expect(item_service).to receive(:item).with({ item_code: 'ITM2' }).and_return(item2)
+      basket.add_item(item_code: 'ITM2')
+      expect(item_service).to receive(:item).with({ item_code: 'ITM1' }).and_return(item1)
+      basket.add_item(item_code: 'ITM1')
+      expect(basket.line_items).to eq([
+                                        LineItem.new(item: item1, count: 1, total_discounted_price: 0.0),
+                                        LineItem.new(item: item2, count: 1, total_discounted_price: 0.0)
+                                      ])
     end
   end
 
