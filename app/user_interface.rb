3f2925ba288338a6c026f17eef556dabb97eb853
Pablo Stahl <stahl@dabbel.eu>
2023-01-11 11:31:26 +0100

    add display table with line items to UI

--- app/user_interface.rb
+++ app/user_interface.rb
@@ -17,7 +17,30 @@ class UserInterface
   sig { void }
   def show_basket
     line_items = @basket.line_items()
-    table = (TTY::Table.new(['The basket is empty', ''], [['Total', '0.00€']]) if line_items.empty?)
+    table = if line_items.empty?
+              TTY::Table.new(['The basket is empty', ''], [['Total', '0.00€']])
+            else
+              create_line_item_table(line_items:)
+            end
     puts(table.render(:ascii))
+  end
+
+  protected
+
+  sig { params(line_items: T::Array[LineItem]).returns(TTY::Table) }
+  def create_line_item_table(line_items:)
+    header = ['Code', 'Name', 'Quantity', 'Unit Price', 'Total']
+    lines = line_items.map do |line_item|
+      currency = line_item.item.currency
+      [
+        line_item.item.code,
+        line_item.item.name,
+        line_item.count,
+        "#{line_item.item.price}#{currency}",
+        "#{line_item.total_discounted_price}#{currency}"
+      ]
+    end
+    total = ['Total', '', '', '', "#{@basket.total_discounted_price}#{T.must(line_items[0]).item.currency}"]
+    TTY::Table.new(header, [:separator].concat(lines.concat([:separator, total])))
   end
 end
--- test/user_interface_spec.rb
+++ test/user_interface_spec.rb
@@ -8,6 +8,18 @@ RSpec::Sorbet.allow_doubles!
 
 RSpec.describe 'UserInterface' do
   let(:basket) { instance_double(Basket) }
+  let(:items) do
+    [
+      instance_double(Item, code: 'ITM1', name: 'Item 1', price: 1.11, currency: '€'),
+      instance_double(Item, code: 'ITM2', name: 'Item 2', price: 1.44, currency: '€')
+    ]
+  end
+  let(:line_items) do
+    [
+      instance_double(LineItem, item: items[0], count: 3, total_discounted_price: 3.33),
+      instance_double(LineItem, item: items[1], count: 1, total_discounted_price: 1.44)
+    ]
+  end
   subject(:user_interface) { UserInterface.new(basket:) }
 
   describe '#show_basket' do
@@ -19,6 +31,25 @@ RSpec.describe 'UserInterface' do
         expect(basket).to receive(:line_items).and_return([])
         expect(TTY::Table).to receive(:new).with(['The basket is empty', ''],
                                                  [['Total', '0.00€']]).and_return(table)
+        expect(table).to receive(:render).with(:ascii).and_return(rendered_table)
+        expect { user_interface.show_basket }.to output("#{rendered_table}\n").to_stdout
+      end
+    end
+
+    context 'when there are items in the basket' do
+      it 'shows a header, the line items in the basket and their total sum' do
+        expect(basket).to receive(:line_items).and_return(line_items)
+        expect(basket).to receive(:total_discounted_price).and_return(4.77)
+        expect(TTY::Table).to receive(:new).with(
+          ['Code', 'Name', 'Quantity', 'Unit Price', 'Total'],
+          [
+            :separator,
+            ['ITM1', 'Item 1', 3, '1.11€', '3.33€'],
+            ['ITM2', 'Item 2', 1, '1.44€', '1.44€'],
+            :separator,
+            ['Total', '', '', '', '4.77€']
+          ]
+        ).and_return(table)
         expect(table).to receive(:render).with(:ascii).and_return(rendered_table)
         expect { user_interface.show_basket }.to output("#{rendered_table}\n").to_stdout
       end
