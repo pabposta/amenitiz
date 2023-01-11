bdebd294ffaf752d616c285d08c5c61886b606e7
Pablo Stahl <stahl@dabbel.eu>
2023-01-11 11:02:20 +0100

    add discount case to calculate_line_item of pricing service

--- app/pricing_service.rb
+++ app/pricing_service.rb
@@ -7,8 +7,21 @@ require_relative 'item'
 class PricingService
   extend T::Sig
 
+  sig do
+    params(datastore_adapter: DatastoreAdapter, discount_factory: DiscountFactory).void
+  end
+  def initialize(datastore_adapter:, discount_factory:)
+    discount_definitions = datastore_adapter.discounts
+    @discounts_by_item_code = discount_definitions.map do |discount_definition|
+      [discount_definition[:item_code], discount_factory.create_discount(discount_definition:)]
+    end.to_h
+  end
+
   sig { params(item: Item, quantity: Integer).returns(Float) }
   def calculate_line_item(item:, quantity:)
-    item.price * quantity
+    price = item.price
+    discount = @discounts_by_item_code[item.code]
+    price = discount.apply(original_price_per_unit: price, quantity:) if discount
+    price * quantity
   end
 end
--- test/pricing_service_spec.rb
+++ test/pricing_service_spec.rb
@@ -11,9 +11,34 @@ RSpec.describe PricingService do
 
   describe '#calculate_line_item' do
     context 'when there are no discounts' do
-      subject(:pricing_service) { PricingService.new }
+      subject(:pricing_service) do
+        datastore_adapter = instance_double(DatastoreAdapter, discounts: [])
+        discount_factory = instance_double(DiscountFactory)
+        PricingService.new(datastore_adapter:,
+                           discount_factory:)
+      end
       it 'calculates the original price if there is no discount' do
         expect(pricing_service.calculate_line_item(item:, quantity: 2)).to eq(6.0)
+      end
+    end
+
+    context 'when there is a discount' do
+      subject(:pricing_service) do
+        datastore_adapter = instance_double(DatastoreAdapter,
+                                            discounts: [{ "name": 'dummy', "item_code": 'ITM1' }])
+        discount = instance_double(Discount, apply: 2.5)
+        discount_factory = instance_double(DiscountFactory, create_discount: discount)
+        PricingService.new(datastore_adapter:,
+                           discount_factory:)
+      end
+
+      it 'applies the discount if it is for the item' do
+        expect(pricing_service.calculate_line_item(item:, quantity: 2)).to eq(5.0)
+      end
+
+      it 'does not apply the discount if it is not for the item' do
+        expect(pricing_service.calculate_line_item(item: instance_double(Item, code: 'ITM2', price: 3.0),
+                                                   quantity: 2)).to eq(6.0)
       end
     end
   end
