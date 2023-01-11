cd55cfb8e73da012a0818203baf9ea239b00ba6a
Pablo Stahl <stahl@dabbel.eu>
2023-01-11 10:44:25 +0100

    add discount interface and BuyXGetYFreeDiscount class

---
+++ app/discount.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+# typed: true
+
+require 'sorbet-runtime'
+
+module Discount
+  extend T::Sig
+  extend T::Helpers
+  interface!
+
+  sig { abstract.params(original_price_per_unit: Float, quantity: Integer).returns(Float) }
+  def apply(original_price_per_unit:, quantity:); end
+end
+
+class BuyXGetYFreeDiscount
+  include Discount
+  extend T::Sig
+
+  sig { params(buy: Integer, get_free: Integer).void }
+  def initialize(buy:, get_free:)
+    @buy = buy.to_i
+    @get_free = get_free.to_i
+    @batch_size = @buy + @get_free
+  end
+
+  sig { override.params(original_price_per_unit: Float, quantity: Integer).returns(Float) }
+  def apply(original_price_per_unit:, quantity:)
+    # calculate the free items in two steps. first, take the ones completing a batch,
+    # e.g., a buy 3 get 2 free batch is made of batches of 5 (3 + 2)
+    full_batch_free_items = quantity / @batch_size * @get_free
+    # then calculate the remaining part, e.g., in the 3 + 2 scenario above,
+    # if we have 9 items, 6 will be paid and 3 will be free
+    # the remainder has to exceed the @buy amount, otherwise it will be negative,
+    # so we need to limit it to 0
+    remaining_free_items = [quantity % @batch_size - @buy, 0].max
+    free_items = full_batch_free_items + remaining_free_items
+    items_to_pay_for = quantity - free_items
+    discounted_total_price = items_to_pay_for * original_price_per_unit.to_f
+    discounted_total_price / quantity
+  end
+end
---
+++ test/discount_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require 'rspec/sorbet'
+require_relative '../app/discount'
+
+RSpec.describe BuyXGetYFreeDiscount do
+  context 'when buying one and getting one free' do
+    subject(:discount) { BuyXGetYFreeDiscount.new(buy: 1, get_free: 1) }
+
+    describe '#apply' do
+      it 'applies 50% discount for an even number of items' do
+        expect(discount.apply(original_price_per_unit: 10.0, quantity: 2)).to eq(5.0)
+        expect(discount.apply(original_price_per_unit: 10.0, quantity: 4)).to eq(5.0)
+        expect(discount.apply(original_price_per_unit: 10.0, quantity: 6)).to eq(5.0)
+      end
+
+      it 'applies no discount for a single item above an even number' do
+        expect(discount.apply(original_price_per_unit: 10.0, quantity: 1)).to eq(10.0)
+        expect(discount.apply(original_price_per_unit: 10.0, quantity: 3)).to eq(20.0 / 3.0)
+        expect(discount.apply(original_price_per_unit: 10.0, quantity: 5)).to eq(6.0)
+      end
+    end
+  end
+
+  context 'when buying three and getting two free' do
+    subject(:discount) { BuyXGetYFreeDiscount.new(buy: 3, get_free: 2) }
+
+    describe '#apply' do
+      it 'applies 40% discount for multiples of 5 items' do
+        expect(discount.apply(original_price_per_unit: 10.0, quantity: 5)).to eq(6.0)
+        expect(discount.apply(original_price_per_unit: 10.0, quantity: 10)).to eq(6.0)
+        expect(discount.apply(original_price_per_unit: 10.0, quantity: 15)).to eq(6.0)
+      end
+
+      it 'applies no discount for a items above a multiple of 5' do
+        expect(discount.apply(original_price_per_unit: 10.0, quantity: 1)).to eq(10.0 / 1.0)
+        expect(discount.apply(original_price_per_unit: 10.0, quantity: 2)).to eq(20.0 / 2.0)
+        expect(discount.apply(original_price_per_unit: 10.0, quantity: 3)).to eq(30.0 / 3.0)
+        expect(discount.apply(original_price_per_unit: 10.0, quantity: 4)).to eq(30.0 / 4.0)
+        expect(discount.apply(original_price_per_unit: 10.0, quantity: 6)).to eq(40.0 / 6.0)
+        expect(discount.apply(original_price_per_unit: 10.0, quantity: 7)).to eq(50.0 / 7.0)
+        expect(discount.apply(original_price_per_unit: 10.0, quantity: 8)).to eq(60.0 / 8.0)
+        expect(discount.apply(original_price_per_unit: 10.0, quantity: 9)).to eq(60.0 / 9.0)
+      end
+    end
+  end
+end
