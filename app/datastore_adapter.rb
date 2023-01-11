5aaa2a81157e72991f11b25575a3b62a6b73449f
Pablo Stahl <stahl@dabbel.eu>
2023-01-11 10:52:45 +0100

    add discounts method to fetch discounts from the datastore adapter

--- app/datastore_adapter.rb
+++ app/datastore_adapter.rb
@@ -15,4 +15,13 @@ class DatastoreAdapter
       Item.new(code: 'CF1', name: 'Coffee', price: 11.23, currency: '€')
     ]
   end
+
+  sig { returns(T::Array[T::Hash[Symbol, T.any(String, T::Hash[Symbol, T.any(Integer, Float)])]]) }
+  def discounts
+    [
+      { name: 'buy_x_get_y_free', item_code: 'GR1', parameters: { buy: 1, get_free: 1 } },
+      { name: 'fixed_price_bulk', item_code: 'SR1', parameters: { buy: 3, discounted_price: 4.5 } },
+      { name: 'fraction_price_bulk', item_code: 'CF1', parameters: { buy: 3, new_price_fraction: 2 / 3.0 } }
+    ]
+  end
 end
--- test/datastore_adapter_spec.rb
+++ test/datastore_adapter_spec.rb
@@ -19,4 +19,17 @@ RSpec.describe DatastoreAdapter do
                                             ])
     end
   end
+
+  describe '#discounts' do
+    it 'returns a hard-coded list of discounts' do
+      expect(datastore_adapter.discounts).to eq([
+                                                  { name: 'buy_x_get_y_free', item_code: 'GR1',
+                                                    parameters: { buy: 1, get_free: 1 } },
+                                                  { name: 'fixed_price_bulk', item_code: 'SR1',
+                                                    parameters: { buy: 3, discounted_price: 4.5 } },
+                                                  { name: 'fraction_price_bulk', item_code: 'CF1',
+                                                    parameters: { buy: 3, new_price_fraction: 2 / 3.0 } }
+                                                ])
+    end
+  end
 end
